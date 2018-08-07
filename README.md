682e25b532d70a3e65181a49ae2cbacd1b8d3af8
Ron Dekker <redkenrok@gmail.com>
2018-08-07 16:33:49 +0200

    Initial code

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # hoast-changed
-Hoast module used filter out files which have not been changed since the last build.
+
+Filter out files which have not been changed since the last build.
+
+> As the name suggest this is a [hoast](https://github.com/hoast/hoast#readme) module.
+
+> Do note this module can only be used if `remove` property of the hoast options is **NOT** set to `true`.
+
+## Usage
+
+Install [hoast-changed](https://npmjs.com/package/hoast-changed) using [npm](https://npmjs.com).
+
+```
+$ npm install hoast-changed
+```
+
+### Parameters
+
+* `file` **{{String}}**: The file path where the update times are stored.
+	* Default: `hoast-changed`
+* `patterns` **{Array|strings}**: A string or an array of strings which gets used to match files using glob patterns. If the file path matches one of the patterns it will never be filtered out regardless of the last update time. See [nanomatch](https://github.com/micromatch/nanomatch#readme) for more details on the patterns.
+	* Required: `no`
+
+### Example
+
+**CLI**
+
+```json
+{
+  "modules": {
+	"hoast-changed": {
+      "patterns": "**/index.md"
+	},
+	"read": {}
+  }
+}
+```
+
+> All the files will be checked for their changed time except the `index.md` files which will always be processed.
+
+**Script**
+
+```javascript
+const Hoast = require('hoast');
+const read = Hoast.read,
+      changed = require('hoast-changed');
+
+Hoast(__dirname)
+  .use(changed({
+    patterns: '**/index.md'
+  }))
+  .use(read())
+  .process();
+```
+
+> All the files will be checked for their changed time except the `index.md` files which will always be processed.
+
+## License
+
+[ISC license](https://github.com/hoast/hoast-changed/blob/master/LICENSE)
---
+++ index.js
@@ -0,0 +1,121 @@
+// Node modules.
+const assert = require('assert'),
+	  { readFile, writeFile } = require('fs'),
+	  { dirname, join } = require('path');
+// If debug available require it.
+let debug; try { debug = require('debug')('hoast-changed'); } catch(error) { debug = function() {}; }
+
+/**
+ * Validates the module options.
+ * @param {Object} options The options.
+ */
+const validateOptions = function(options) {
+	assert(typeof(options) === 'object', 'hoast-changed: options must be of type object.');
+	if (options.file) {
+		assert(typeof(options.file) === 'string', 'hoast-changed: file must be of type string.');
+	}
+	if (options.patterns) {
+		assert(typeof(options.patterns) === 'string' || (Array.isArray(options.patterns) && options.patterns.length > 0 && typeof(options.patterns[0] === 'string')), 'hoast-changed: patterns must be of type string or an array of strings.');
+	}
+};
+
+/**
+ * Reads the file from storage.
+ * @param {String} path File path.
+ */
+const read = function(path) {
+	return new Promise(function(resolve, reject) {
+		readFile(path, 'utf8', function(error, data) {
+			if (error) {
+				return reject(error);
+			}
+			resolve(JSON.parse(data));
+		});
+	});
+};
+
+/**
+ * Writes JS object to storage.
+ * @param {Object} hoast Hoast instance.
+ * @param {String} path File path.
+ * @param {Object} content Object to write to storage.
+ */
+const write = function(hoast, path, content) {
+	return new Promise(function(resolve, reject) {
+		hoast.helper.createDirectory(dirname(path))
+			.then(function() {
+				writeFile(path, JSON.stringify(content), 'utf8', function(error) {
+					if (error) {
+						return reject(error);
+					}
+					resolve();
+				})
+			})
+			.catch(function(error) {
+				reject(error);
+			});
+	});
+};
+
+/**
+ * Convert the content of files.
+ * @param {Object} options The module options.
+ */
+module.exports = function(options) {
+	debug(`Initializing module.`);
+	
+	validateOptions(options);
+	debug(`Validated options.`);
+	options = Object.assign({
+		file: 'hoast-changed'
+	}, options);
+	
+	return async function(hoast, files) {
+		debug(`Running module.`);
+		
+		const path = join(hoast.options.destination, file);
+		// Read changed list from storage.
+		// This is done each batch as it is otherwise retained in memory even across multiple process calls.
+		let list;
+		try {
+			list = await read(path);
+			debug(`Read changed list from '${path}'`);
+		} catch(error) {
+			list = {};
+			debug(`Unable to read changed list from '${path}', created new list instead.`);
+		}
+		
+		debug(`Start filtering files`);
+		// Loop through the files.
+		const filtered = files.filter(function(file) {
+			debug(`Filtering file '${file.path}'.`);
+			
+			// If it matches any of the patterns always process.
+			if (nanomatch.any(file.path, options.patterns)) {
+				debug(`File path matched patterns.`);
+				return true;
+			}
+			// If it is in the list and not changed since the last time do not process.
+			if (list[file.path] && file.stats.ctimeMs <= list[file.path]) {
+				debug(`File no changed since last process.`);
+				return false;
+			}
+			debug(`File never processed before..`);
+			// Update changed time and process.
+			list[file.path] = file.stats.ctimeMs;
+			return true;
+		});
+		debug(`Finished filtering files.`);
+		
+		// Write list to storage.
+		try {
+			await write(hoast, path, list);
+		} catch(error) {
+			throw error;
+		}
+		debug(`Wrote changed list to '${path}'.`);
+		
+		// Return filtered files.
+		return filtered;
+	};
+};
